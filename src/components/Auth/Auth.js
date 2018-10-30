2365c336cbed3bd4153616721bdfc148b6b33b22
Anthony Romero <drumnbass302@yahoo.com>
2018-10-30 17:03:15 -0500

    Server Running

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class Auth extends Component {
+    render(){
+        console.log(this.props);
+        return <div>
+            Auth
+        </div>
+    }
+}
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Nav(props) { 
+    console.log(this.props);
+            // {this.props.location.pathname !== "/" ?
+            return <div>
+                Nav
+            </div>
+            // ): null
+         
+}
